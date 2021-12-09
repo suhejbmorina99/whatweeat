c7258cc4cd25e9a6f1f29f4f3d28180903e10f94
ebi <you@example.com>
2021-12-09 22:00:43 +0100

    with backend

---
+++ backend/app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+require("dotenv/config");
+
+app.use(cors());
+app.options("*", cors());
+
+//middleware
+app.use(express.json());
+app.use(morgan("tiny"));
+
+//Routers
+
+// const categoriesRoutes = require('./routes/categories')
+const productsRouters = require("./routers/products");
+// const usersRoutes = require('./routes/users')
+
+const api = process.env.API_URL;
+
+app.use(`${api}/products`, productsRouters);
+
+//Database connection
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "qapohajm",
+  })
+  .then(() => {
+    console.log("Database Connecting Success");
+  })
+  .catch((err) => {
+    console.log("Connecting lost...");
+  });
+
+//Server
+app.listen(3000, () => {
+  console.log("Server is started now");
+});
---
+++ backend/models/product.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    default: "",
+  },
+  price: {
+    type: Number,
+    default: 0,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+});
+
+productSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
+
+productSchema.set("toJSON", {
+  virtuals: true,
+});
+
+exports.Product = mongoose.model("Product", productSchema);
