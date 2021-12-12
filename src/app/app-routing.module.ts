5133f2c70662c1ae2971d2db1c5b23fbb1932820
ebi <you@example.com>
2021-12-12 20:24:27 +0100

    the lastest verion of beta 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,7 @@
-import { SlidersComponent } from './sliders/sliders.component';
-import { PickedFoodComponent } from './picked-food/picked-food.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CardsComponent } from './cards/cards.component';
 
-const routes: Routes = [
-  { path: 'header', component: HeaderComponent },
-  { path: 'cards', component: CardsComponent },
-  { path: 'footer', component: FooterComponent },
-  { path: 'pickedFood', component: PickedFoodComponent },
-  { path: 'sliders', component: SlidersComponent },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { SlidersComponent } from './sliders/sliders.component';
-import { CardsComponent } from './cards/cards.component';
-import { PickedFoodComponent } from './picked-food/picked-food.component';
+
 import { CardModule } from 'primeng/card';
 
 import {ButtonModule} from 'primeng/button';
-import { FooterComponent } from './footer/footer.component';
+
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    SlidersComponent,
-    CardsComponent,
-    PickedFoodComponent,
-    FooterComponent,
     HomeComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, CardModule, ButtonModule],
